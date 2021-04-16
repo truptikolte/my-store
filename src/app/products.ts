76fdb377ceb4cec480c89f7aff093dcfdeda19c1
truptikolte <tnkoltegmail.com>
2021-04-16 11:29:42 +0100

    Display Product List

---
+++ src/app/products.ts
@@ -0,0 +1,26 @@
+export const products = [
+{
+    id: 1,
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with best screen'
+},
+{
+    id: 2,
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A greate phone with best camera'
+},
+{
+    id: 3,
+    name: 'Phone Stadered',
+    price: 399,
+    description: ''
+},
+{
+    id: 4,
+    name: 'Moto G',
+    price: 750,
+    description: 'A phone with all features'
+},
+]
